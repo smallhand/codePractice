c8bc2a6c16365e3150774faf4bdd9ba0761b9a2e
py <pc00083@gmail.com>
2016-08-25 17:03:19 +0800

    add a010

---
+++ zerojudge/a010/a010.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <string.h>
+using namespace std;
+
+
+int main(){
+	string str;
+    while (getline(cin,str)){
+        int sum=0;
+        bool word=false;
+        str[str.size()]='!';
+        for (int i=0; i<str.size()+1; i++){
+            if((str[i]>=65&&str[i]<=90)||(str[i]>=97&&str[i]<=122)){
+                word=true;
+            }
+            else if(word==true){
+                sum++;
+                word=false;
+            }
+        }
+        cout << sum << endl;
+    }
+
+	return 0;
+}
+
+
