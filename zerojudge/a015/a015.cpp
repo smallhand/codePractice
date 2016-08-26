3e35d428183d62d840ee19369ba6d3f20cfae685
py <pc00083@gmail.com>
2016-08-26 11:55:03 +0800

    inverse matrix

---
+++ zerojudge/a015/a015.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m,n,i,j;
+    while(cin>>i>>j)
+    {
+       int T[i][j];
+       for(m=0;m<i;m++){
+        for(n=0;n<j;n++)      
+          cin>>T[m][n];
+       }
+       for(m=0;m<j;m++){
+        for(n=0;n<i;n++)
+          cout<<T[n][m]<<" ";
+        cout<<endl;
+       } 
+     
+    }
+    return 0;
+}
