ae408bed1ea9082c15a82ea2161dd77be77f3267
py <pc00083@gmail.com>
2016-08-26 12:34:21 +0800

    practice string

---
+++ zerojudge/a022/a022.cpp
@@ -0,0 +1,28 @@
+//Palindrome (迴文)
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+
+{
+    string n;
+    while ( cin>>n )
+    { int judge=1;
+      for ( int i=0; i<n.size()/2; i++ )
+          { if (n[i]!=n[n.size()-1-i])
+               { judge=0;
+                 cout << "no" << endl;
+                 break;
+               }
+            else { judge=1;
+                   continue;
+                 }
+          }
+        if (judge==1)
+            cout << "yes" << endl;
+    }
+
+}
