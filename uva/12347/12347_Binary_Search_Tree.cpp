61e94d829404073251daa3455108a52ed721be7c
py <pc00083@gmail.com>
2019-02-22 18:42:22 +0800

    Build the binary search tree, and then output by postorder method

---
+++ uva/12347/12347_Binary_Search_Tree.cpp
@@ -0,0 +1,70 @@
+/*
+Build the binary search tree, then output by postorder method
+Date: 2019/02/22
+*/
+#include <iostream>
+using namespace std;
+
+struct node {
+    int value;
+    node *left = nullptr;
+    node *right = nullptr;
+};
+
+node *insert(node *head, int new_value){
+    node *N = new node;
+    N->value = new_value;
+
+    if (head==nullptr){
+        return N;
+    }
+
+    node *tree = head;
+    node *next = head;
+    while(next!=nullptr) { // repeat traveling until leaf
+        tree = next;
+        if (new_value > next->value) {
+            next = next -> right;
+        }
+        else {
+            next = next -> left;
+        }
+    }
+
+    if (new_value > tree->value) {
+        tree->right = N;
+    }
+    else {
+        tree->left = N;
+    }
+    return head;
+}
+
+void pre_order(node *head) {
+    if (head==nullptr) return;
+    printf("%d\n", head->value);
+    pre_order(head->left);
+    pre_order(head->right);
+}
+void in_order(node *head) {
+    if (head==nullptr) return;
+    in_order(head->left);
+    printf("%d\n", head->value);
+    in_order(head->right);
+}
+void post_order(node *head) {
+    if (head==nullptr) return;
+    post_order(head->left);
+    post_order(head->right);
+    printf("%d\n", head->value);
+}
+
+int main(){
+    int value;
+    node *head = nullptr;
+    while (cin>>value){
+        head = insert(head, value);
+    }
+    //pre_order(head);
+    post_order(head);
+}
