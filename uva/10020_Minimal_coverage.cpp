be22650ce7879f4ba51e5f4652aabacac0c9be08
py <py@pyde-MacBook-Pro.local>
2019-03-03 22:31:04 +0800

    greedy algorithm

--- uva/10020_Minimal_coverage.cpp
+++ uva/10020_Minimal_coverage.cpp
@@ -1,3 +1,6 @@
+/*
+Date: 2019/03/03
+*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -32,8 +35,7 @@ int main(){
 
             int left = 0;
             int index = 0, Max=0;
-            int flag;
-            int i;
+            int i, flag;
             vector<Segment> ans;
 
             while(Max < M) {
@@ -59,36 +61,6 @@ int main(){
                     printf("%d %d\n", ans[i].left, ans[i].right);
                 }
             }
-
-            /*
-            for (int i=0; i<pairs; i++) {
-                flag = -1;
-                for(; i<pairs && line[i].left<=left; i++) {
-                    if(line[i].right > right) {
-                        right = line[i].right;
-                        flag = i;
-                    }
-                }
-                if (flag==-1) { // 所有線段右邊都沒辦法覆蓋住M
-                    break; 
-                }
-                ans.push_back(line[flag]);
-                if (right >= M) { // find the ans, can stop~
-                    break;
-                }
-                left = right; // [0~M] -> [right, M]
-                i--;
-            }
-            if (right<M){
-                cout << "0\n";
-            }
-            else {
-                cout << ans.size() << endl;
-                for (int i=0; i<ans.size(); i++) {
-                    printf("%d %d\n", ans[i].left, ans[i].right);
-                }
-            }
-            */
 
             if(cases) // 連續兩outputs要多空一行
                 cout <<  endl;
