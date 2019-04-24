91ad4807b2a8cb97278f297674546b9763623aa9
py <py@pyde-MacBook-Pro.local>
2019-04-24 11:18:50 +0800

    cpe, gpe (modify for loop order)

--- uva/353_Pesky_Palindromes.cpp
+++ uva/353_Pesky_Palindromes.cpp
@@ -19,12 +19,24 @@ bool isPal(string str){
     return true;
 }
 
-int countN(string str_in){
+int countN2(string str_in){
     vector<string> palList;
     for (int len=1; len<str_in.length()+1; len++) {
         for (int i=0; i < str_in.length()+1-len; i++ ){
             // if the substring isn't in the palList, and it is palindromes, then push into the palList
             if (find(palList.begin(), palList.end(), str_in.substr(i, len)) == palList.end() && isPal(str_in.substr(i, len))) { // substr: the ith postion, and str length=len
+                palList.push_back(str_in.substr(i, len));
+            }
+        }
+    }
+    return palList.size();
+}
+
+int countN(string str_in ) {
+    vector<string> palList;
+    for (int i=0; i<str_in.size(); i++) {
+        for (int len=1; len<=str_in.size()-i; len++) {
+            if (isPal(str_in.substr(i,len)) && find(palList.begin(), palList.end(), str_in.substr(i, len))==palList.end() ) {
                 palList.push_back(str_in.substr(i, len));
             }
         }
